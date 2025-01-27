d325d67c8ba273586911414de8537f0acf06c93f
Artem Ptashenko <aptashenko2019@gmail.com>
2025-01-27 12:54:34 +0100

    changes

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import pg from 'pg';
 import {SUPABASE_PASSWORD, SUPABASE_URL} from "./configs.js";
 
-const sequelize = new Sequelize(`postgresql://postgres:${SUPABASE_PASSWORD}@db.${SUPABASE_URL}.supabase.co:5432/postgres`, {
+const sequelize = new Sequelize(`postgresql://postgres:${SUPABASE_PASSWORD}@db.${SUPABASE_URL}/postgres`, {
     dialectModule: pg,
     schema: 'public',
     dialectOptions: {
